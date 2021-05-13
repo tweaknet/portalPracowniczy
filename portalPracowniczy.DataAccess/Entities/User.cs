cf6d300a5e5e5f67d67b726ad2e5f067e5f077d8
Marcin Chmielewski <mc@tweaknet.pl>
2021-05-13 21:15:09 +0200

    Dodaj pliki projektów.

---
+++ portalPracowniczy.DataAccess/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        public int IdERP { get; set; }
+        public int IdRCP { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public int IdPosition { get; set; }
+        public int IdBranch { get; set; }
+    }
+}
