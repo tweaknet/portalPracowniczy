5bf87d73ab372f1083bcf9decb9311891a249fa3
Marcin Chmielewski <mc@tweaknet.pl>
2021-05-13 21:26:52 +0200

    łączenie z bazą

--- portalPracowniczy.DataAccess/PortalStorageContextFactory.cs
+++ portalPracowniczy.DataAccess/PortalStorageContextFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace portalPracowniczy.DataAccess
 {
-    class PortalStorageContextFactory
+    class PortalStorageContextFactory : IDesignTimeDbContextFactory<PortalStorageContext>
     {
+        public PortalStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PortalStorageContext>();
+            //optionsBuilder.UseSqlServer("Provider=SQLNCLI11;Data Source=.\\sqlexpress;Integrated Security=SSPI;Initial Catalog=WebApp");
+            optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=portal;Integrated Security=True");
+            return new PortalStorageContext(optionsBuilder.Options);
+        }
     }
 }
