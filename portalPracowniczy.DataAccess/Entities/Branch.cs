d2f49a38f685e8ef8eb208147f971aeed076d456
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2021-05-14 09:11:06 +0200

    branch, position

---
+++ portalPracowniczy.DataAccess/Entities/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.Entities
+{
+    class Branch : EntityBase
+    {
+        public string Name { get; set; }
+        public int IdSuperior { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
---
+++ portalPracowniczy.DataAccess/Entities/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.Entities
+{
+    class Position : EntityBase
+    {
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
