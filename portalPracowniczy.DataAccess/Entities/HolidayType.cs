7eff79dad9528960194428752f84ab92c6d2ccce
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2021-05-14 09:16:22 +0200

    holidayTYpe

---
+++ portalPracowniczy.DataAccess/Entities/HolidayType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.Entities
+{
+    class HolidayType : EntityBase
+    {
+        public string Name { get; set; }
+        public int IdRCP { get; set; }
+        public int IdERP { get; set; }
+        public List<Holiday> Holidays { get; set; }
+        public List<Proposal> Proposals { get; set; }
+    }
+}
