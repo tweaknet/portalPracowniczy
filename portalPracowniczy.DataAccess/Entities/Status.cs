6753ef1a59a3c957a6f5e0e26cdc0e30f5a409f5
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2021-05-14 09:05:22 +0200

    status

---
+++ portalPracowniczy.DataAccess/Entities/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.Entities
+{
+    class Status : EntityBase
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public List<Proposal> Proposals { get; set; }
+
+    }
+}
