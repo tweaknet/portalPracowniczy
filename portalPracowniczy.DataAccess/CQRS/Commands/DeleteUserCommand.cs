a3fbacdfbcca2a7429da0ba2d030192534014eac
Marcin Chmielewski <mc@tweaknet.pl>
2021-11-07 12:58:45 +0100

    początek delete

---
+++ portalPracowniczy.DataAccess/CQRS/Commands/DeleteUserCommand.cs
@@ -0,0 +1,19 @@
+﻿using portalPracowniczy.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portalPracowniczy.DataAccess.CQRS.Commands
+{
+    public class DeleteUserCommand : CommandBase<User, User>
+    {
+        public override async Task<User> Execute(PortalStorageContext context)
+        {
+            context.Users.Remove(this.Parameter);
+            await context.SaveChangesAsync();
+            return this.Parameter;
+        }
+    }
+}
